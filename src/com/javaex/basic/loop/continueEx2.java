6707662fa197c8c0f8460418dc8647c310cf1702
하이미디어학원 <@DESKTOP-T8SG68P>
2024-03-19 16:34:06 +0900

    2024_03_19

--- src/com/javaex/basic/loop/continueEx2.java
+++ src/com/javaex/basic/loop/continueEx2.java
@@ -2,13 +2,15 @@ package com.javaex.basic.loop;
 
 public class continueEx2 {
 	public static void main(String[] args) {
-		ex1();
-		ex2();
+//		ex1();
+//		ex2();
 //		ex3();
+		ex4();
+//		ex5();
 	}
 
 	public static void ex1() {
-		for (int dan = 1; dan <= 9; dan++) {
+		for (int dan = 2; dan <= 9; dan++) {
 			System.out.println(dan + "단");
 			for (int i = 1; i <= 9; i++) {
 				System.out.println(dan + " * " + i + " = " + dan * i);
@@ -17,7 +19,7 @@ public class continueEx2 {
 	}
 
 	public static void ex2() {
-		int i = 1;
+		int i = 2;
 		int n = 1;
 		while (i < 10) {
 			n = 1;
@@ -33,7 +35,34 @@ public class continueEx2 {
 	}
 
 	public static void ex3() {
-		int m = 8;
+		int j = 6;
+		for (int i = 1; i <= j; i++) {
+			for (int n = 0; n < i; n++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+	public static void ex4() {
+		int i = 1;
+		int n = 1;
+		while (i <= 6) {
+			n = 1;
+			while (n <= i) {
+				n += 1;
+				System.out.print("*");
+
+			}
+			i += 1;
+			System.out.println("");
+
+		}
+
+	}
+
+	public static void ex5() {
+		int m = 7;
 		for (int j = 1; j <= m; j++) {
 
 			for (int l = m - 1; l >= j; l--) {
